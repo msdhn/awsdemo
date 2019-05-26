d29266bbd8319853defb58004e8c1e8bee9520d7
Masud Hasan <masudbook@gmail.com>
2019-05-26 19:06:06 +0200

    commit

---
+++ aws-s3/src/main/java/eu/msdhn/aws/awss3/AwsS3Application.java
@@ -0,0 +1,13 @@
+package eu.msdhn.aws.awss3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsS3Application.class, args);
+	}
+
+}
